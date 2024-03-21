932308c9746fb74afe69fd8f9f4641987d81eb3b
Jozef Sukovsky <sukovskyj@gmail.com>
2024-03-21 22:29:40 +0100

    add async fastapi implementation

---
+++ 005_diff_api_perf/part1/fastapiapp-async/main.py
@@ -0,0 +1,95 @@
+from datetime import datetime
+import os
+from typing import List
+
+from dotenv import load_dotenv
+from fastapi import Depends, FastAPI, Request
+
+from pydantic import BaseModel
+
+from sqlalchemy import MetaData, create_engine, func, join
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.future import select
+from sqlalchemy.orm import Session, sessionmaker
+
+
+load_dotenv()
+
+dbi = os.getenv('DATABASE_URL') or 'nah'
+sync_engine = create_engine(dbi, pool_size=10, max_overflow=10)
+async_engine = create_async_engine(
+    dbi.replace('sql://', 'sql+asyncpg://'), future=True, pool_size=10, max_overflow=10)
+metadata = MetaData()
+metadata.reflect(sync_engine, only=['testapp_parent', 'testapp_child'])
+Base = automap_base(metadata=metadata)
+Base.prepare()
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession)
+
+Parent = Base.classes.testapp_parent
+Child = Base.classes.testapp_child
+
+app = FastAPI()
+
+
+class ParentSchema(BaseModel):
+
+    title: str
+    created: datetime
+    modified: datetime
+
+
+class ChildSchema(BaseModel):
+
+    title: str
+    json_field: dict
+    long_text: str
+    created: datetime
+    modified: datetime
+    parent: ParentSchema
+
+
+class Response(BaseModel):
+    count: int
+    results: List[ChildSchema]
+    previous: str
+    next: str
+
+
+@app.get("/children", response_model=Response)
+async def read_children(request: Request, page: int = 1):
+    async with SessionLocal() as db:
+        per_page = 30
+        offset = (page - 1) * per_page
+        query = (
+            select(func.count())
+            .select_from(join(Child, Parent))
+        )
+        result = await db.execute(query)
+        count = result.scalar()
+        if count == 0:
+            return {'count': 0, 'results': [], 'previous': None, 'next': None}
+
+        pages = count // per_page
+        if count % per_page != 0:
+            pages += 1
+        payload = []
+        query = (
+            select(Child, Parent)
+            .select_from(join(Child, Parent))
+            .order_by(Child.id)
+            .limit(30)
+            .offset(offset)
+        )
+        result = await db.stream(query)
+
+        async for row in result:
+            child, parent = row
+            child.parent = parent
+            payload.append(child)
+
+        base_url = f'{request.url.scheme}://{request.client.host}:{request.scope["server"][1]}'
+        next_page = f'{base_url}?page={page + 1}' if page < pages else None
+        previous_page = f'{base_url}?page={page - 1}' if page > 1 else None
+        return {'count': count, 'results': payload, 'previous': previous_page, 'next': next_page}
---
+++ 005_diff_api_perf/part2/fastapiapp-async/main.py
@@ -0,0 +1,14 @@
+import json
+
+from fastapi import FastAPI, Request
+
+
+test_data = {}
+with open('./test_data.json', encoding='utf8') as fd:
+    test_data = json.loads(fd.read())
+
+app = FastAPI()
+
+@app.get("/children")
+async def read_children(_request: Request):
+    return test_data
